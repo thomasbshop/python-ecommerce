a5f715d2cc36a9fd35d6a212f85cc5363b9ddbeb
thomas <thomasbshop@gmail.com>
2019-03-18 00:49:56 +0300

    sessions

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_home(request):
+	# print(request.session)
+	# print(dir(request.session))
+	# request.session.set_expiry(300) 
+	key = request.session.session_key
+	print(key)
 	return render(request, "carts/home.html", {})
