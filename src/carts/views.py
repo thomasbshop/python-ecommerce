4dcbf3a402f1816e6c9824bb73cce71f5010047f
thomas <thomasbshop@gmail.com>
2019-03-19 02:27:09 +0300

    cart model and cart views

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart_create(user=None):
+	cart_obj = Cart.objects.create(user=None)
+	print("New cart created.")
+	return cart_obj
+
 def cart_home(request):
-	# print(request.session)
-	# print(dir(request.session))
-	# request.session.set_expiry(300) 
-	key = request.session.session_key
-	print(key)
+	request.session['cart_id'] = "12"
+	cart_id = request.session.get("cart_id", None)
+	#if cart_id is None:
+	#	cart_obj = cart_create()
+	#	request.session['cart_id'] = cart_obj.id
+	#else: 
+	qs = Cart.objects.filter(id=cart_id)
+	if qs.count == 1:
+		print('Cart ID exists')
+		cart_obj = qs.first()
+	else:
+		cart_obj = cart_create()
+		request.session['cart_id'] = cart_obj.id
 	return render(request, "carts/home.html", {})
