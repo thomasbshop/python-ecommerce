ae2c6e59bf5c1c0630f373fc89514606d1ef8ce5
thomas <thomasbshop@gmail.com>
2019-03-22 20:30:58 +0300

    cart update view

--- src/carts/models.py
+++ src/carts/models.py
@@ -8,27 +8,27 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
-	def new_or_get(self, request):
-		cart_id = request.session.get("cart_id", None)
-		qs = self.get_queryset().filter(id=cart_id)
-		if qs.count() == 1:
-			new_obj = False
-			cart_obj = qs.first()
-			if request.user.is_authenticated() and cart_obj.user is None:
-				cart_obj.user = request.user
-				cart_obj.save()
-			else:
-				cart_obj = Cart.objects.new(user=request.user)
-				new_obj = True
-				request.session['cart_id'] = cart_obj.id
-			return cart_obj, new_obj
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
 
-	def new(self, user=None):
-		user_obj = None
-		if user is not None:
-			if user.is_authenticated():
-				user_obj = user
-		return self.model.objects.create(user=user_obj)
+    def new(self, user=None):
+    	user_obj = None
+    	if user is not None:
+    		if user.is_authenticated():
+    			user_obj = user
+    	return self.model.objects.create(user=user_obj)
 
 class Cart(models.Model):
 	user      = models.ForeignKey(User, null=True, blank=True)
@@ -59,7 +59,10 @@ m2m_changed.connect(m2m_changed_cart_reciver, sender=Cart.products.through)
 
 
 def pre_save_cart_reciver(sender, instance, *args, **kwargs):
-	instance.total = instance.subtotal + 10
+    if instance.subtotal > 0:
+        instance.total = instance.subtotal + 10
+    else:
+        instance.total = 0.00
 
 
 pre_save.connect(pre_save_cart_reciver, sender=Cart)
---
+++ src/carts/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+
+# own
+
+from .views import (
+    cart_home, 
+    cart_update,
+    )
+
+urlpatterns = [
+    url(r'^$', cart_home, name="home"),
+    url(r'^update/$', cart_update, name="update"),
+]
+
+
+
