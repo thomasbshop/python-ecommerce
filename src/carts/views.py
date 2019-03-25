10a7b251e685dd5c968331cc96800b1cc10b9584
thomas <thomasbshop@gmail.com>
2019-03-26 02:53:52 +0300

    cart icon

--- src/carts/views.py
+++ src/carts/views.py
@@ -24,6 +24,6 @@ def cart_update(request):
 			cart_obj.products.remove(product_obj)
 		else:
 			cart_obj.products.add(product_obj)
-
+		request.session['cart_count'] = cart_obj.products.count()
 		# return redirect(product_obj.get_absolute_url())
 	return redirect("cart:home")
