5af3735f928d7a5b16225c56b660541b10ff65de
thomas <thomasbshop@gmail.com>
2019-03-27 17:12:36 +0300

    generate Order ID

--- src/ecommerce/utils.py
+++ src/ecommerce/utils.py
@@ -12,6 +12,18 @@ http://joincfe.com/blog/random-string-generator-in-python/
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+def unique_order_id_generator(instance):
+    """
+    This is for a Django project with an order_id field.
+    """
+    order_id_new = random_string_generator().upper()
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(order_id=order_id_new).exists()
+    if qs_exists:
+        return unique_slug_generator(instance, new_slug=new_slug)
+    return order_id_new
+
 def unique_slug_generator(instance, new_slug=None):
     """
     This is for a Django project and it assumes your instance 
